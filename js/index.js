c09c0f9201b333196c3b9aebaf3d71db7a5af855
marwanabubakr <dev@marobakr>
2023-11-18 20:04:00 +0200

    v-2

--- js/index.js
+++ js/index.js
@@ -111,3 +111,32 @@ const currentDate = new Date().getFullYear()
 const yearElement = (document.getElementById(
   'year'
 ).innerHTML += `  ${currentDate} `)
+
+/* Scroll to top button */
+const button = document.querySelector('.scrollToTopBtn')
+
+const displayButton = () => {
+  window.addEventListener('scroll', () => {
+    console.log(window.scrollY)
+
+    if (window.scrollY > 100) {
+      button.style.display = 'block'
+    } else {
+      button.style.display = 'none'
+    }
+  })
+}
+
+const scrollToTop = () => {
+  button.addEventListener('click', () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    })
+    console.log(event)
+  })
+}
+
+displayButton()
+scrollToTop()
