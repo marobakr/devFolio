c978257f41be1d866ebffaed5929b014ebc2d1f0
marwanabubakr <dev@marobakr>
2023-11-20 12:13:15 +0200

    last build

--- js/index.js
+++ js/index.js
@@ -24,89 +24,7 @@ var swiper = new Swiper('.mySwiper', {
   },
 })
 
-/* Add Toggle Class Active */
-var links = document.querySelectorAll('nav ul a')
-links.forEach(function (link) {
-  link.addEventListener('click', function () {
-    links.forEach(function (innerLink) {
-      innerLink.classList.remove('active')
-    })
-    link.classList.add('active')
-  })
-})
-
-window.addEventListener('scroll', function () {
-  var scrollPosition = window.scrollY
-
-  sections.forEach(function (section) {
-    var offset = section.offsetTop
-    var height = section.offsetHeight
-    if (scrollPosition >= offset && scrollPosition < offset + height ) {
-      links.forEach(function (innerLink) {
-        innerLink.classList.remove('active')
-        console.log('uyes')
-      })
-      var correspondingLink = document.querySelector(
-        'nav a[href="#' + section.id + '"]'
-      )
-      correspondingLink.classList.add('active')
-    }
-  })
-})
-
-/* Aniamtion Typeing */
-var sections = document.querySelectorAll('section')
-var typeText = document.querySelector('.typeText')
-var textToBeTyped = 'software engineer'
-var textToBeTypedArr = [' software engineer', ' Freelancer', ' Designer']
-var index = 0,
-  isAdding = true,
-  textToBeTypedIndex = 0
-
-function playAnim() {
-  setTimeout(
-    function () {
-      // set the text of typeText to a substring of the text to be typed using index.
-      typeText.innerText = textToBeTypedArr[textToBeTypedIndex].slice(0, index)
-      if (isAdding) {
-        // adding text
-        if (index > textToBeTypedArr[textToBeTypedIndex].length) {
-          // no more text to add
-          isAdding = false
-          typeText.classList.add('showAnim')
-          //break: wait 2s before playing again
-          setTimeout(function () {
-            typeText.classList.remove('showAnim')
-            playAnim()
-          }, 2000)
-          return
-        } else {
-          // increment index by 1
-          index++
-        }
-      } else {
-        // removing text
-        if (index === 0) {
-          // no more text to remove
-          isAdding = true
-          //switch to next text in text array
-          textToBeTypedIndex =
-            (textToBeTypedIndex + 1) % textToBeTypedArr.length
-        } else {
-          // decrement index by 1
-          index--
-        }
-      }
-      // call itself
-      playAnim()
-    },
-    isAdding ? 120 : 60
-  )
-}
-// start animation
-playAnim()
-
-// function Get Year of footer 
+// function Get Year of footer
 const currentDate = new Date().getFullYear()
 const yearElement = (document.getElementById(
   'year'
@@ -126,7 +44,6 @@ const displayButton = () => {
     }
   })
 }
-
 const scrollToTop = () => {
   button.addEventListener('click', () => {
     window.scroll({
@@ -140,3 +57,29 @@ const scrollToTop = () => {
 
 displayButton()
 scrollToTop()
+
+// Counter Timing
+const Counter = () => {
+  window.addEventListener('scroll', () => {
+    console.log(window.scrollY)
+    if (window.scrollY >= 2500 && window.scrollY <= 2550) {
+      // COunter Timer
+      var counters = document.getElementsByClassName('counterTimer')
+
+      for (var i = 0; i < counters.length; i++) {
+        handleCounter(counters[i])
+      }
+      function handleCounter(counter) {
+        var c = 0
+        var interval = setInterval(() => {
+          if (c == counter.dataset.count) {
+            clearInterval(interval)
+          }
+          counter.innerText = c
+          c++
+        }, 2)
+      }
+    }
+  })
+}
+Counter()
